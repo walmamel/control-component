870cf035f23e0ead3357cca3187c69d48b7fde4b
walmamel <waleskamelchert@gmail.com>
2019-11-04 14:15:04 +0100

    Updates

---
+++ precache-manifest.633e66f879928b2b74aa657ae0281f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169081182c61e785db4606ec93fc12e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37a9ea4dcfe87e9a82e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "044b3288f984415b722e",
+    "url": "/static/css/main.eef012a6.chunk.css"
+  },
+  {
+    "revision": "b37a9ea4dcfe87e9a82e",
+    "url": "/static/js/2.bb0a81be.chunk.js"
+  },
+  {
+    "revision": "044b3288f984415b722e",
+    "url": "/static/js/main.03e4df95.chunk.js"
+  },
+  {
+    "revision": "0d56ebf8640dd214e562",
+    "url": "/static/js/runtime-main.242b68bb.js"
+  },
+  {
+    "revision": "09d2ebacb358263d8642bdd3928b0ce7",
+    "url": "/static/media/dandelion.09d2ebac.jpg"
+  }
+]);
